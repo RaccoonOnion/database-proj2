0504cb452c7b2e924503d00ef33633b730585821
faintcicado <zhouyujie0326@163.com>
2023-05-20 16:54:11 +0800

    ...

--- CS307/src/project2/Test.java
+++ CS307/src/project2/Test.java
@@ -3,12 +3,8 @@ package project2;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.*;
 import java.sql.*;
-import java.util.Date;
-import java.util.StringTokenizer;
 
 public class Test {
 
@@ -17,8 +13,13 @@ public class Test {
     private static String phone;
     private static boolean logined = false;
 
-    private static Connection con = null;
+    protected static Connection con = null;
     private static Statement stmt1 = null;
+
+    private static final int ACCOUNT_COUNT = 10000;
+    private static final int POST_COUNT = 10000;
+    private static final int PHONE_LENGTH = 11;
+    private static final int LFS_COUNT = 10000;
 
 
     private static void openDB(Properties prop) {
@@ -42,7 +43,6 @@ public class Test {
             System.exit(1);
         }
     }
-
 
     private static void closeDB() {
         if (con != null) {
@@ -68,14 +68,6 @@ public class Test {
         }
     }
 
-    private static List<String> loadCSVFile(String fileName) {
-        try {
-            return Files.readAllLines(Path.of("resources/" + fileName + ".csv"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public static void main(String[] args) {
 
 
@@ -84,30 +76,14 @@ public class Test {
         int cntTotal = 0;
 
         openDB(prop);
+        Fundamental fundamental = new Fundamental(con);
 
-        ArrayList<String> names = generateNames(10000);
-        ArrayList<String> phones = generatePhones(10000);
-        ArrayList<String> titles = generateTitles(10000);
-        ArrayList<String> contents = generateContents(10000);
-        ArrayList<String> cities = generateCities(10000);
-
-        // 执行插入操作
-        for (int i = 0; i < names.size(); i++) {
-            String name = names.get(i);
-            String phone = phones.get(i);
-            String title = titles.get(i);
-            String content = contents.get(i);
-            String city = cities.get(i);
-
-           fundamental.regisiterNewUser(name, phone,con,stmt1);
-           fundamental.posting(title, content, city, name,con,stmt1);
-            fundamental.likePost(name, i + 1,con,stmt1);
-            fundamental.favoritePost(name, i + 1,con,stmt1);
-            fundamental.sharePost(name, i + 1,con,stmt1);
-        }
-
-        // 输出测试结果
-        System.out.println("Test data generated and inserted successfully.");
+//        accountInserting(fundamental);
+//        postInserting(fundamental);
+//        likedInserting(fundamental);
+//        sharedInserting(fundamental);
+//        favoredInserting(fundamental);
+//        followInserting(fundamental);
 
         closeDB();
 
@@ -118,77 +94,87 @@ public class Test {
 
     }
 
-    static class QReader {
-        private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        private StringTokenizer tokenizer = new StringTokenizer("");
-
-        private String innerNextLine() {
-            try {
-                return reader.readLine();
-            } catch (IOException e) {
-                return null;
-            }
+    private static void accountInserting(Fundamental fundamental){
+        // Generate and insert account data
+        for (int i = 0; i < ACCOUNT_COUNT; i++) {
+            String name = generateRandomName();
+            String phone = generateRandomPhone(PHONE_LENGTH);
+            fundamental.registerNewUser(name, phone);
         }
+        System.out.println("Test data generated and inserted successfully.");
+    }
+    public static void postInserting(Fundamental fundamental) {
+        ArrayList<String> accountNames = fundamental.getAllAccountNames();
+        Random random = new Random();
+        for (int i = 0; i < POST_COUNT; i++) {
+            String title = generateRandomString(20);
+            String content = generateRandomString(100);
+            String city = generateRandomCity();
+            String name = accountNames.get(random.nextInt(accountNames.size()));
+            fundamental.posting(title, content, city, name);
+        }
+    }
+    public static void likedInserting(Fundamental fundamental) {
+        ArrayList<String> accountNames = fundamental.getAllAccountNames();
+        ArrayList<Integer> postIds = fundamental.getAllPostIds();
 
-        public boolean hasNext() {
-            while (!tokenizer.hasMoreTokens()) {
-                String nextLine = innerNextLine();
-                if (nextLine == null) {
-                    return false;
+        Random random = new Random();
+
+        for (int i = 0; i < LFS_COUNT; i++) {
+            String name = accountNames.get(random.nextInt(accountNames.size()));
+            int postId = postIds.get(random.nextInt(postIds.size()));
+            fundamental.likePost(name, postId);
+        }
+        System.out.println("Liked data inserted successfully.");
+    }
+    public static void sharedInserting(Fundamental fundamental) {
+        ArrayList<String> accountNames = fundamental.getAllAccountNames();
+        ArrayList<Integer> postIds = fundamental.getAllPostIds();
+
+        Random random = new Random();
+
+        for (int i = 0; i < LFS_COUNT; i++) {
+            String name = accountNames.get(random.nextInt(accountNames.size()));
+            int postId = postIds.get(random.nextInt(postIds.size()));
+            fundamental.sharePost(name, postId);
+        }
+        System.out.println("Shared data inserted successfully.");
+    }
+    public static void favoredInserting(Fundamental fundamental) {
+        ArrayList<String> accountNames = fundamental.getAllAccountNames();
+        ArrayList<Integer> postIds = fundamental.getAllPostIds();
+
+        Random random = new Random();
+
+        for (int i = 0; i < LFS_COUNT; i++) {
+            String name = accountNames.get(random.nextInt(accountNames.size()));
+            int postId = postIds.get(random.nextInt(postIds.size()));
+            fundamental.favoritePost(name, postId);
+        }
+        System.out.println("Favored data inserted successfully.");
+    }
+    public static void followInserting(Fundamental fundamental) {
+        ArrayList<String> accountNames = fundamental.getAllAccountNames();
+        Random random = new Random();
+        for (String follower : accountNames) {
+            // Generate random number of followings for each follower
+            int numFollowings = random.nextInt(accountNames.size() - 1); // Randomly select number of followings
+            HashSet<String> followingSet = new HashSet<>();
+            while (followingSet.size() < numFollowings) {
+                String following = accountNames.get(random.nextInt(accountNames.size())); // Randomly select a following
+                if (!following.equals(follower) && !followingSet.contains(following)) {
+                    followingSet.add(following);
                 }
-                tokenizer = new StringTokenizer(nextLine);
             }
-            return true;
-        }
+            // Insert followings into follow table
 
-        public String nextLine() {
-            tokenizer = new StringTokenizer("");
-            return innerNextLine();
-        }
-
-        public String next() {
-            hasNext();
-            return tokenizer.nextToken();
-        }
-
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        public long nextLong() {
-            return Long.parseLong(next());
-        }
-    }
-
-    static class QWriter implements Closeable {
-        private BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        public void print(Object object) {
-            try {
-                writer.write(object.toString());
-            } catch (IOException e) {
-                return;
-            }
-        }
-
-        public void println(Object object) {
-            try {
-                writer.write(object.toString());
-                writer.write("\n");
-            } catch (IOException e) {
-                return;
-            }
-        }
-
-        @Override
-        public void close() {
-            try {
-                writer.close();
-            } catch (IOException e) {
-                return;
+            for (String following : followingSet) {
+                fundamental.followUser(follower, following);
             }
         }
     }
+
+
 
     public static String generateRandomPhone(int length) {
         String numberChar = "0123456789";
@@ -210,97 +196,46 @@ public class Test {
         return sb.toString();
     }
 
-    public static String getRandomTime(String startDate, String endDate) {
-        //时间的格式
-        String dateTime = null;
-        long result = 0;
-        SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
-        //转化
-        java.util.Date start = null;
-        try {
-            start = format1.parse(startDate);
-            Date end = format1.parse(endDate);
-            //获取建立时间
-            result = start.getTime() + (long) (Math.random() * (end.getTime() - start.getTime()));
-            SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            dateTime = format2.format(result);
-            return dateTime;
-        } catch (ParseException e) {
-            e.printStackTrace();
+    private static String generateRandomName() {
+        String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            char c = characters.charAt(random.nextInt(characters.length()));
+            sb.append(c);
         }
-        return null;
+        return sb.toString();
     }
 
-    private static final String[] NAMES = {"John", "Alice", "Michael", "Emily", "Daniel", "Olivia", "David", "Sophia"};
-    private static final String[] CITIES = {"New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia"};
-
-    // 生成随机姓名
-    private static ArrayList<String> generateNames(int count) {
-        ArrayList<String> names = new ArrayList<>();
+    private static String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ';l,.!@#$%^&*(){}[],.<>?/??》";
+        StringBuilder sb = new StringBuilder(length);
         Random random = new Random();
-        for (int i = 0; i < count; i++) {
-            int index = random.nextInt(NAMES.length);
-            names.add(NAMES[index]);
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            char randomChar = characters.charAt(index);
+            sb.append(randomChar);
         }
-        return names;
+        return sb.toString();
     }
 
-    // 生成随机手机号
-    private static ArrayList<String> generatePhones(int count) {
-        ArrayList<String> phones = new ArrayList<>();
+    private static String generateRandomCity() {
+        String[] cities = { "New York", "London", "Tokyo", "Paris", "Berlin", "Sydney", "Toronto", "Dubai" ,
+                "北京", "上海", "广州", "深圳", "杭州", "南京", "重庆", "成都", "武汉", "西安",
+                "天津", "苏州", "济南", "青岛", "大连", "沈阳", "长春", "哈尔滨", "长沙", "南昌",
+                "福州", "厦门", "南宁", "珠海", "佛山", "东莞", "海口", "三亚", "兰州", "银川",
+                "太原", "西宁", "贵阳", "昆明", "拉萨", "乌鲁木齐", "香港", "澳门", "台北", "高雄",
+                "桂林", "黄山", "张家界", "九寨沟", "凤凰古城", "西塘", "苏州园林", "乌镇", "周庄", "壶口瀑布",
+                "敦煌", "莫高窟", "泰山", "庐山", "衡山", "黄山", "峨眉山", "武当山", "五台山", "恒山",
+                "青岛", "大连", "天津", "烟台", "威海", "潍坊", "济宁", "泰安", "临沂", "滨州",
+                "菏泽", "日照", "德州", "聊城", "东营", "枣庄", "淄博", "莱芜", "赣州", "上饶",
+                "景德镇", "萍乡", "九江", "新余", "鹰潭", "宜春", "吉安", "抚州", "南通", "常州",
+                "无锡", "扬州", "镇江", "泰州", "盐城", "连云港", "徐州", "淮安", "宿迁", "湖州"};
         Random random = new Random();
-        for (int i = 0; i < count; i++) {
-            StringBuilder phone = new StringBuilder("1");
-            for (int j = 0; j < 10; j++) {
-                int digit = random.nextInt(10);
-                phone.append(digit);
-            }
-            phones.add(phone.toString());
-        }
-        return phones;
-    }
-
-    // 生成随机标题
-    private static ArrayList<String> generateTitles(int count) {
-        ArrayList<String> titles = new ArrayList<>();
-        Random random = new Random();
-        for (int i = 0; i < count; i++) {
-            int length = random.nextInt(5) + 5; // 随机标题长度为5到10之间
-            StringBuilder title = new StringBuilder();
-            for (int j = 0; j < length; j++) {
-                char c = (char) (random.nextInt(26) + 'A');
-                title.append(c);
-            }
-            titles.add(title.toString());
-        }
-        return titles;
-    }
-
-    // 生成随机内容
-    private static ArrayList<String> generateContents(int count) {
-        ArrayList<String> contents = new ArrayList<>();
-        Random random = new Random();
-        for (int i = 0; i < count; i++) {
-            int length = random.nextInt(20) + 10; // 随机内容长度为10到30之间
-            StringBuilder content = new StringBuilder();
-            for (int j = 0; j < length; j++) {
-                char c = (char) (random.nextInt(26) + 'a');
-                content.append(c);
-            }
-            contents.add(content.toString());
-        }
-        return contents;
-    }
-
-    // 生成随机城市
-    private static ArrayList<String> generateCities(int count) {
-        ArrayList<String> cities = new ArrayList<>();
-        Random random = new Random();
-        for (int i = 0; i < count; i++) {
-            int index = random.nextInt(CITIES.length);
-            cities.add(CITIES[index]);
-        }
-        return cities;
+        int index = random.nextInt(cities.length);
+        return cities[index];
     }
 }
+
+
 
